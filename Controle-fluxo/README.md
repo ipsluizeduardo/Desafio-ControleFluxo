570ecc9aa545edc0c7e493190fed8c75b8f931af
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-09-21 11:20:33 -0300

    Desafio completo

---
+++ Controle-fluxo/README.md
@@ -0,0 +1,3 @@
+## Desafio DIO
+Controle de fluxo.
+
---
+++ Controle-fluxo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parametro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch(ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if(parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroUm - parametroDois;
+
+        for(int i = 1;i <= contagem; i++) {
+            System.out.println("Imprimindo o numero " + i);
+        }
+    }
+}
